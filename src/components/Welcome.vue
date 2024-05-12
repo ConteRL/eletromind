c8a875d91d89b757a5faeeb9885c20d06c2cad6b
ConteRL <rodrigo.oliveira2003@outlook.com>
2024-05-12 15:25:44 -0300

    Improving Welcome

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="home-start" class="container-fluid">
+  <div id="home-start" class="container-fluid d-flex  align-items-center">
 
     <div class="row align-items-center h-75">
-      <div class="col-5 mx-0 px-0">
+      <div id="welcome-text" class="col-6 px-5">
         <div class="text-start text-light">
           <h1 id="welcome-phrase">Welcome!!</h1>
           <h2>Come and learn more about electronics and computing.</h2>
         </div>
       </div>
 
-      <div class="col-5 mx-0 px-4 ali">
+      <div class="col-6 px-5">
         <img src="../assets/circuits/parallel-leds.png" id="welcome-img" class="rounded-4 border border-3 p-2" alt="LEDs in parallel">
       </div>
     </div>
@@ -30,22 +30,26 @@ export default {
   height: 80vh;
   position: relative;
   background-color: #2B3A41;
+  background-image: url("./../assets/home_circuit_bg.png");
+  background-repeat: no-repeat;
+  background-position: right center;
+  background-size: cover;
 }
 
-.col-4 {
-  background: blue;
+#welcome-text {
+  text-shadow: 1px 2px 2px #00A2E2, 0 0 0.1em white, 0 0 0.1em white;
 }
 
 #welcome-img {
   width: 100%;
   height: 100%;
-
   border-color: #00A2E2 !important;
 }
 
 #welcome-phrase {
   font-family: 'Fugaz One';
   color: white;
-
 }
+
+
 </style>
